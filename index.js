41ab71ec33d161abb48286f73313a3527cdc5ec2
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-02-26 14:48:42 +0530

    Session code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
-import FunctionalComp from './components/FunctionalComp';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-//AppRegistry.registerComponent(appName, () => FunctionalComp);
