36ba87ce808e8beb044f739152c90133fcdad3e4
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-03-02 10:45:11 +0530

    reviewing flex property

--- App.js
+++ App.js
@@ -3,36 +3,57 @@ import { StyleSheet,Text,View, SafeAreaView } from 'react-native';
 class App extends React.Component{
   render(){
     return (
-      <SafeAreaView>
-        <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        
+        <View style={styles.containerrow}>
           <Text style={styles.textOne}> One </Text>
           <Text style={styles.textTwo}> Two </Text>
           <Text style={styles.textThree}> Three </Text>
-          <Text style={styles.textFour}> Four </Text>
         </View> 
+        
+        <View style={styles.containercolumn}>
+          <Text style={styles.textOne}> One </Text>
+          <Text style={styles.textTwo}> Two </Text>
+          <Text style={styles.textThree}> Three </Text>
+        </View>
+
       </SafeAreaView>
     );
   }
 };
 
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1, 
+    flex:1,
     flexDirection:'column',
     backgroundColor: '#ddd',
   },
+  containerrow: {
+    flex:1,
+    flexDirection:'row',
+  },
+  containercolumn: {
+    flex:1,
+    flexDirection:'column',
+  },
   textOne: {
-    padding:10, backgroundColor: 'yellow'
+    flex:1,
+    padding:10, 
+    backgroundColor: '#edeef7'
   },
-  textTwo: {
-    padding:10, backgroundColor: 'orange'
+  
+  textTwo: { 
+    flex:1,
+    padding:10, 
+    backgroundColor: '#ffefa1'
   },
+  
   textThree: {
-    padding:10, backgroundColor: 'red'
+    flex:1, 
+    padding:10, 
+    backgroundColor: '#6ddccf'
   },
-  textFour: {
-    padding:10, backgroundColor: 'coral'
-  }
 });
 
 export default App;
