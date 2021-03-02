a139f3f386c8e16b004ce459eb9db8081b568a0a
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-03-02 10:21:48 +0530

    Basic Style

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Text,View, Button } from 'react-native';
+import { StyleSheet,Text,View, SafeAreaView } from 'react-native';
 class App extends React.Component{
-
   render(){
     return (
-      <View> </View>  
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.textOne}> One </Text>
+          <Text style={styles.textTwo}> Two </Text>
+          <Text style={styles.textThree}> Three </Text>
+          <Text style={styles.textFour}> Four </Text>
+        </View> 
+      </SafeAreaView>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    flexDirection:'column',
+    backgroundColor: '#ddd',
+  },
+  textOne: {
+    padding:10, backgroundColor: 'yellow'
+  },
+  textTwo: {
+    padding:10, backgroundColor: 'orange'
+  },
+  textThree: {
+    padding:10, backgroundColor: 'red'
+  },
+  textFour: {
+    padding:10, backgroundColor: 'coral'
+  }
+});
 
 export default App;
