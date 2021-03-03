3fe710ead1ca0e5a4f5c4a26b44634de7f907503
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-03-03 11:43:50 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
 import React from 'react';
-import { Text,View, Button } from 'react-native';
+import { Text,View } from 'react-native';
 import Counter from './components/Counter';
 
 class App extends React.Component{
 
   constructor(props){
     super(props);
-    console.log('Parent Log : Parent Constructor Initiated!');
   }
-
-  state = {counter: 0}
-  
-  increment = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-
-  componentDidMount(){
-    console.log('Parent Log : Parent Component Mounted!');
-  }
-
-  componentDidUpdate(prevState){
-    if(this.state.counter !== prevState.counter){
-      console.log('Parent Log : Parent State Updated');
-    }
-  }
-
 
   render(){
-    console.log('Parent Log : Parent Render Method Running!');
     return (
-      <View style={{justifyContent: 'center', alignItems:'center', padding:20}}>
-        <Button onPress={this.increment}
-        title ="Counter ++">
-          </Button>
-        <Counter counter = {this.state.counter}/>
+      <View>
+          <Text> Hello! </Text>
       </View>  
     );
   }
