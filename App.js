61135dbc5908dbdc0f4e6b30355c6a65ac8e2377
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-03-08 23:30:18 +0530

    ios device setup done

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import SliderComp from './src/components/SwitchComp';
 import WebViewComp from './src/components/WebViewComp';
-import Home from './src/components/Home';
 import FetchData from './src/components/FetchData';
 
 class App extends React.Component{
