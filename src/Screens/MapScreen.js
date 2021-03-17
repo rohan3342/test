ce1aec19f51d44d23c12820e27292c360db345a9
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-03-17 18:52:47 +0530

    Map component updated

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -10,6 +10,43 @@ class MapScreen extends Component {
       latitude: 0,
       longitude: 0,
       coordinates: [],
+      markers: [
+        {
+          title: 'Haryana',
+          coordinates: {
+            latitude: 29.0588,
+            longitude: 76.0856,
+          },
+        },
+        {
+          title: 'Lucknow',
+          coordinates: {
+            latitude: 26.8467,
+            longitude: 80.9462,
+          },
+        },
+        {
+          title: 'Jaipur',
+          coordinates: {
+            latitude: 26.9124,
+            longitude: 75.7873,
+          },
+        },
+        {
+          title: 'Kasol',
+          coordinates: {
+            latitude: 32.01,
+            longitude: 77.315,
+          },
+        },
+        {
+          title: 'Hyderabad',
+          coordinates: {
+            latitude: 17.385,
+            longitude: 78.4867,
+          },
+        },
+      ],
     };
   }
 
@@ -37,24 +74,41 @@ class MapScreen extends Component {
     );
   }
 
+  handleRegionChange = (mapData) => {
+    this.setState({
+      latitude: mapData.latitude,
+      longitude: mapData.longitude,
+      mapData,
+    });
+  };
+
   render() {
     return (
       <Fragment>
         <MapView
-          style={{ flex: 1 }}
-          region={{
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          style={styles.mapView}
+          region={this.state.mapData}
+          onRegionChangeComplete={this.handleRegionChange}
           showsUserLocation={true}>
           <Marker
+            pinColor={'green'}
             coordinate={{
               latitude: this.state.latitude,
               longitude: this.state.longitude,
             }}
+            title="Current Location"
+            onDragEnd={(e) => {
+              console.log('dragEnd', e.nativeEvent.coordinate);
+            }}
           />
+          {this.state.markers.map((marker, index) => (
+            <MapView.Marker
+              pinColor={'red'}
+              key={index}
+              coordinate={marker.coordinates}
+              title={marker.title}
+            />
+          ))}
         </MapView>
         <View style={styles.MainTxtContainer}>
           <View style={styles.currentLocView}>
@@ -62,6 +116,19 @@ class MapScreen extends Component {
             <Text>
               {this.state.latitude} , {this.state.longitude}
             </Text>
+          </View>
+          <View>
+            <Text>Multiple Markers List</Text>
+            {this.state.markers.map((data, index) => {
+              return (
+                <View key={index} style={styles.currentLocView}>
+                  <Text>{data.title}</Text>
+                  <Text>
+                    {data.coordinates.latitude} , {data.coordinates.longitude}
+                  </Text>
+                </View>
+              );
+            })}
           </View>
         </View>
       </Fragment>
