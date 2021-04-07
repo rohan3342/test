e45b0d2f4bfb4d2275a74ec9410983e044dd4570
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-04-07 13:34:07 +0530

    Vector Icons implemented

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { Text,View,StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
-class App extends React.Component{
+class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  render(){
+  render() {
     return (
-      <View>
-          <Text> Hello! </Text>
-      </View>  
+      <View style={styles.container}>
+        <Icon name="home" size={100} color="green" />
+        <Text style={styles.Icontxt}> Hello </Text>
+      </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Icontxt: {
+    fontSize: 40,
+    fontWeight: '600',
+    color: 'green',
+  },
+});
 
 export default App;
