07619843bc291b91a8bc85f165f7bec0eaa9a9f9
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-04-08 19:05:08 +0530

    Audio Player Screen complete

---
+++ src/components/PlayerComp.js
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import TrackPlayer, {
+  useTrackPlayerProgress,
+  usePlaybackState,
+  useTrackPlayerEvents,
+} from 'react-native-track-player';
+
+const ControlButton = ({ title, size, color, onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={styles.controlButtonContainer}>
+    <Ionicons name={title} size={size} color={color} />
+  </TouchableOpacity>
+);
+
+const ProgressBar = () => {
+  const progress = useTrackPlayerProgress();
+  return (
+    <View style={styles.progressBar}>
+      <View style={{
+        flex: progress.position,
+        backgroundColor: '#191919'
+      }} />
+      <View
+        style={{
+          flex: progress.duration - progress.position,
+          backgroundColor: 'grey',
+        }}
+      />
+    </View>
+  );
+};
+
+const PlayerComp = (props) => {
+  const { onNext, onPrevious, onTogglePlayback } = props;
+  const playbackState = usePlaybackState();
+  const [trackTitle, setTrackTitle] = useState('');
+  const [trackArtwork, setTrackArtwork] = useState();
+  const [trackArtist, setTrackArtist] = useState('');
+  const [bufferState, setBufferState] = useState(false);
+
+  useTrackPlayerEvents(['playback-track-changed'], async event => {
+    if (event.type === TrackPlayer.TrackPlayerEvents.PLAYBACK_TRACK_CHANGED) {
+      const track = await TrackPlayer.getTrack(event.nextTrack);
+      const { title, artist, artwork } = track || {};
+      setTrackTitle(title);
+      setTrackArtist(artist);
+      setTrackArtwork(artwork);
+    }
+  });
+
+  let middleButton = 'play-circle-outline';
+  if (playbackState === TrackPlayer.STATE_PLAYING ||
+    playbackState === TrackPlayer.STATE_BUFFERING) {
+    middleButton = 'ios-pause-circle-outline';
+  }
+
+  // useEffect(() => {
+  //   getBufferState();
+  //   console.log(bufferState);
+  // }, []);
+
+  // const getBufferState = () => {
+  //   if (TrackPlayer.STATE_BUFFERING) setBufferState(true);
+  // }
+
+  return (
+    <View style={styles.conatiner}>
+      <View style={styles.albumArtWrapper}>
+        {
+          trackArtwork !== undefined ?
+            (<Image
+              style={styles.albumArt}
+              source={{ uri: trackArtwork }} />) :
+            (<MaterialIcons name="music-box-outline" size={280} color="#191919" />)
+        }
+      </View>
+
+      {
+        trackTitle !== '' ?
+          (
+            <>
+              <Text style={styles.titleTxt}>{trackTitle}</Text>
+              <Text style={styles.artistTxt}>{trackArtist}</Text>
+            </>
+          ) : (
+            <Text style={styles.titleTxt}>Tap on Play Button!</Text>
+          )
+      }
+      <ProgressBar />
+      <View style={styles.controls}>
+        <ControlButton
+          onPress={onPrevious}
+          color="#191919"
+          size={60}
+          title="play-skip-back-circle-outline" />
+        <ControlButton
+          onPress={onTogglePlayback}
+          color="#191919"
+          size={100}
+          title={middleButton} />
+        <ControlButton
+          onPress={onNext}
+          color="#191919"
+          size={60}
+          title="play-skip-forward-circle-outline" />
+      </View>
+
+    </View>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  conatiner: {
+    width: '85%',
+    height: '80%',
+    alignItems: 'center',
+    borderRadius: 20,
+    backgroundColor: '#954B4D',
+  },
+  albumArtWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 30,
+    width: '80%',
+    height: '50%',
+  },
+  albumArt: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+  },
+  titleTxt: {
+    fontSize: 24,
+  },
+  artistTxt: {
+    fontSize: 20,
+    fontWeight: '300',
+  },
+  progressBar: {
+    backgroundColor: '#191919',
+    height: 4,
+    borderRadius: 50,
+    width: '90%',
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  controls: {
+    marginTop: 15,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  controlButtonContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+
+export default PlayerComp;
--- src/screens/AudioScreen.js
+++ src/screens/AudioScreen.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import PlayerComp from '../components/PlayerComp';
+import TrackPlayer, { usePlaybackState } from 'react-native-track-player';
+import playlistData from '../utils/playlistData.json'
 
 class AudioScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() { }
+
+  trackPlayerSetup = async () => {
+    await TrackPlayer.setupPlayer({});
+    await TrackPlayer.updateOptions({
+      stopWithApp: true,
+      capabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+        TrackPlayer.CAPABILITY_STOP,
+      ],
+      compactCapabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+      ]
+    });
+  }
+
+  togglePlayback = async () => {
+    const currentTrack = await TrackPlayer.getCurrentTrack();
+    const playbackState = await TrackPlayer.getState();
+    if (currentTrack == null) {
+      await TrackPlayer.reset();
+      await TrackPlayer.add(playlistData);
+      await TrackPlayer.play();
+    } else {
+      if (playbackState === TrackPlayer.STATE_PAUSED) {
+        await TrackPlayer.play();
+      } else {
+        await TrackPlayer.pause();
+      }
+    }
+  }
+
+  skipToNext = async () => {
+    try {
+      await TrackPlayer.skipToNext();
+    } catch (error) { console.error(error); }
+  }
+
+  skipToPrevious = async () => {
+    try {
+      await TrackPlayer.skipToPrevious();
+    } catch (error) { console.error(error); }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>AudioScreen</Text>
+        <PlayerComp
+          onNext={this.skipToNext}
+          onPrevious={this.skipToPrevious}
+          onTogglePlayback={this.togglePlayback}
+        />
       </View>
     );
   }
@@ -16,6 +74,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#191919'
   },
 });
 
