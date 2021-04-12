a0ffbeb812706db23194b93829471c36050afbbc
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-04-12 15:24:50 +0530

    GoogleSign Button UI working

--- App.js
+++ App.js
@@ -1,18 +1,108 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5'
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure();
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      userInfo: {},
+      isUserLoggedIn: false,
+      isLoginScreenPresented: undefined,
+      currentUser: {},
+    }
+  }
+
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo, isUserLoggedIn: true });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('SIGN_IN_CANCELLED');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('IN_PROGRESS');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('PLAY_SERVICES_NOT_AVAILABLE');
+      } else {
+        console.log('ELSE Block');
+      }
+    }
+  };
+
+  getCurrentUserInfo = async () => {
+    try {
+      const userInfo = await GoogleSignin.signInSilently();
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+        // user has not signed in yet
+      } else {
+        // some other error
+      }
+    }
+  };
+
+  isSignedIn = async () => {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    this.setState({ isLoginScreenPresented: !isSignedIn });
+  };
+
+  getCurrentUser = async () => {
+    const currentUser = await GoogleSignin.getCurrentUser();
+    this.setState({ currentUser });
+  };
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      this.setState({ userInfo: null, isUserLoggedIn: false }); // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  revokeAccess = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      console.log('deleted');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getCurrentUserInfo();
   }
 
   render() {
+    const { isUserLoggedIn, userInfo } = this.state;
+    console.log(userInfo.user);
     return (
       <View style={styles.container}>
         <Icon name="home" size={100} color="green" />
         <Text style={styles.Icontxt}> Hello </Text>
+        {
+          isUserLoggedIn ?
+            (<Text>Hello {userInfo.user.givenName}</Text>)
+            :
+            (<GoogleSigninButton
+              style={{ width: 220, height: 60 }}
+              size={GoogleSigninButton.Size.Wide}
+              color={GoogleSigninButton.Color.Light}
+              onPress={this.signIn}
+            />)
+        }
       </View>
     );
   }
